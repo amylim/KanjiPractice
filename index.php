05bfcaa1d6de4dc7451539294440a9205efc52c2
Amy Lim <amy.lim.cs@gmail.com>
2015-01-25 08:07:06 -0500

    Changed the index page so that it has some images

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
-<a href="flashcard.php">Flashcard Practice</a><br>
-<a href="kanji_list.php">Kanji List</a><br>
-<a href="anki_download.html">Anki download</a><br>
+<style type="text/css">
+table, th, td {
+    padding: 50px;
+    text-align:center;
+}
+
+#ex3 { 
+line-height: 0px; 
+color: transparent; 
+font-size: 50px; 
+font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, sans-serif; 
+font-weight: 300; 
+text-transform: uppercase; 
+-webkit-transition: all 0.5s ease; 
+-moz-transition: all 0.5s ease; 
+-o-transition: all 0.5s ease; 
+} 
+
+#ex3:hover { 
+line-height: 120px; 
+color: #575858; 
+} 
+
+#ex3 img { 
+float: left; 
+margin: 0 15px; 
+}
+</style>
+
+<table align="center">
+    <tr>
+        <td>
+            <a id="ex3" href="flashcard.php"><img src='image/pencil.png'/>Practice</a>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <a id="ex3" href="kanji_list.php"><img src='image/note.png'/>Kanji List</a>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <a id="ex3" href="anki_download.html"><img src='image/star.png'/>Anki Download</a>
+        </td>
+    </tr>
+</table>
