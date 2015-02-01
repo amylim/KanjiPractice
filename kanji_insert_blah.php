35d174edd5a9d9db42960d51170042aa78e71c63
Amy Lim <amy.lim.cs@gmail.com>
2015-02-01 01:47:33 -0500

    Added insert and update templates for KanjiCharacter and KanjiCompoud

--- kanji_insert_blah.php
+++ kanji_insert_blah.php
@@ -5,10 +5,28 @@ exit();
 include("include/kanjidb_admin_connect.php"); 
 
 mysql_query("SET NAMES utf8");
+
+// change quere here
+
+// KanjiCharacter
+//$sql = "
+//    INSERT INTO KanjiCharacter (id, kanji)
+//    VALUES 
+//    (31,'夢'),
+//(40,'良');
+//";
 $sql = "
-    DELETE FROM KanjiCharacter
-    WHERE rid=4;
-";
+    INSERT INTO KanjiCompound (related_kanji, expression, reading, meaning)
+    VALUES
+    (31,'夢','ゆめ','dream'),
+(40,'不良','ふりょう','delinquent');
+
+    ";
+//$sql = "
+//    UPDATE KanjiCompound 
+//    SET meaning='forest'
+//    WHERE RID=68;
+//";
 
 if ($connection->query($sql) === TRUE) {
     echo "New record created successfully";
